8d97ddc959203aac55da7a57aa4f6c5f0aea7ff9
Haemulramen <apple11078@gmail.com>
2024-05-02 17:31:56 +0900

    CHORE: Article 모델 필드 수정

--- articles/models.py
+++ articles/models.py
@@ -11,6 +11,8 @@ from django.contrib.auth.models import AbstractUser
 #     category = models.CharField(choices=CHOICE, max_length=10, unique=True)
 
 class Article(models.Model):
+    title = models.TextField(verbose_name="제목")
     text = models.TextField(verbose_name="기사 내용")
     # category = models.ForeignKey(Interest, verbose_name="카테고리", on_delete=models.CASCADE)
     company = models.CharField(max_length=30, verbose_name="언론사")
+    link = models.TextField(verbose_name="원본 링크")
