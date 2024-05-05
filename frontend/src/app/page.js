8ffb4f9cbaf5c8f5b584e2ff6aa408762d436ebd
SongMinhyuk <dnfldpden32@gmail.com>
2024-05-05 18:03:05 +0900

    Modified: page.js 상위 10개만 가져오는 것으로 변경

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,15 +1,17 @@
-import { fetchData, getServerSideProps } from "./lib/fetch_db";
+import { getServerSideProps } from "./lib/fetch_db";
 
 export default function Home() {
   const data = getServerSideProps();
   return (
-    <main className="py-20">
+    <main className="py-20 px-2 text-center">
+      <h1 className=" text-4xl">최근 10개의 뉴스를 가져왔어요</h1>
       {data.then((response) =>
         response.props.data.map((item, index) => {
           const news_link = "/detail/" + item.id;
+          if (index >= 10) return;
           return (
             <a href={news_link}>
-              <section className="flex items-center justify-between m-2">
+              <section className="flex items-center justify-between m-2 border p-3">
                 <h1 className=" text-lg">{item.title}</h1>
                 <p>{item.company}</p>
               </section>
