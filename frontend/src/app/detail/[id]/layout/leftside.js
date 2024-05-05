5afa5bc27df4009c2dd481292228f4959c727b10
SongMinhyuk <dnfldpden32@gmail.com>
2024-05-05 18:03:45 +0900

    FEAT: 상세페이지 좌우 나뉘어 표기하는 layout 각각 생성

---
+++ frontend/src/app/detail/[id]/layout/leftside.js
@@ -0,0 +1,7 @@
+export default function LeftSide({ children }) {
+  return (
+    <div className=" justify-center text-center  w-1/2 border brac">
+      {children}
+    </div>
+  );
+}
---
+++ frontend/src/app/detail/[id]/layout/rightside.js
@@ -0,0 +1,3 @@
+export default function RightSide({ children }) {
+  return <div className=" text-center w-1/2">{children}</div>;
+}
