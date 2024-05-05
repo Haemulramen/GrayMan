57cfc6e8afb09d2aa21633a4885dc2f15aa567e1
SongMinhyuk <dnfldpden32@gmail.com>
2024-05-05 18:02:41 +0900

    Modified: 뉴스 상세페이지 DB에서 받아오도록 설정

--- frontend/src/app/detail/[id]/page.js
+++ frontend/src/app/detail/[id]/page.js
@@ -1,7 +1,24 @@
+import LeftSide from "./layout/leftside";
+import RightSide from "./layout/rightside";
+import { getServerSideProps } from "./lib/fetch_db";
+
 export default function Detail(props) {
+  const data = getServerSideProps(props.params.id);
   return (
     <main className="py-20">
-      <h1>{props.params.id}</h1>
+      <LeftSide>
+        {data.then((response) =>
+          response.props.data.map((item, index) => {
+            return (
+              <>
+                <p>GPT가 요약한 내용입니다!</p>
+                <p className="">{item.text}</p>
+              </>
+            );
+          })
+        )}
+      </LeftSide>
+      <RightSide></RightSide>
     </main>
   );
 }
