5e42ef00422aab517cb0ab075bf96d071ec196ca
Haemulramen <apple11078@gmail.com>
2024-06-10 19:52:20 +0900

    FEAT: before moderation

---
+++ comments/utils.py
@@ -0,0 +1,10 @@
+# from openai import OpenAI
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
+# class CreateComment():
+#     def __init__(self):
+#         self.client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+        
