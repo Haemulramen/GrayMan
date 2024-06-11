252eb7484cd5b232c6070af3f973f8d32726b403
Haemulramen <apple11078@gmail.com>
2024-06-11 18:55:45 +0900

    FEAT: comment moderation

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Comment
+from .utils import CreateComment
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+    def validate_content(self, value):
+        commment_checker = CreateComment()
+        toxicity = commment_checker.comment_moderation(value)
+        
+        if toxicity == False:
+            return value
+        else:
+            raise serializers.ValidationError("This comment is toxic")
