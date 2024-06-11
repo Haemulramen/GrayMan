1727a58ebed2d7e1b745ac33bbdce7f9f72b7f1d
jse <73538772+jse8406@users.noreply.github.com>
2024-06-11 23:55:18 +0900

    DB reset

--- articles/insert_summary.py
+++ articles/insert_summary.py
@@ -7,16 +7,14 @@ con = sqlite3.connect("db.sqlite3")
 cur = con.cursor()
 cur.execute("SELECT id, text from articles_article")
 rows = cur.fetchall()
-count = 0
+
 for row in rows:
-    if count == 3:
-        break
     row_id = row[0]
+    if row_id <= 14:
+        continue
     text = row[1]
     sum = cn.summarize(text)
     correction = cn.correction(text, sum)
-    print(correction)
-    count += 1
-    # cur.execute("INSERT INTO articles_summary (text, correction, origin_id) VALUES (?, ?, ?)", (sum, correction, row_id))
-    # con.commit()
-# con.close()
+    cur.execute("INSERT INTO articles_summary (text, correction, origin_id) VALUES (?, ?, ?)", (sum, correction, row_id))
+    con.commit()
+con.close()
