9845911f3bde0f3aa339e29a6c3066597e32c99b
SongMinhyuk <dnfldpden32@gmail.com>
2024-06-12 21:49:53 +0900

    FIX: DB fetch함수 파라미터 쿼리로 변경

--- frontend/src/app/detail/[id]/page.js
+++ frontend/src/app/detail/[id]/page.js
@@ -5,12 +5,16 @@ import App from "./lib/chat";
 import Statistics from "./lib/statistics";
 
 export default function Detail(props) {
-  const summaryData = getServerSideProps(props.params.id, "articles_summary");
-  const originData = getServerSideProps(props.params.id, "articles_article");
+  const { id } = props.params;
+  const summaryData = getServerSideProps(
+    `SELECT * FROM articles_summary WHERE origin_id = ${id}`
+  );
+  const originData = getServerSideProps(
+    `SELECT * FROM articles_article WHERE id = ${id}`
+  );
 
   return (
     <main className="grid grid-cols-12 gap-4 py-20 bg-white">
-
       <div className="col-span-4 p-4 text-left">
         <div>
           {summaryData.then((response) =>
@@ -19,10 +23,14 @@ export default function Detail(props) {
               let splited = correction.split(/\s*(?=\d+\.\s)/);
               return (
                 <div key={index} className="mb-6 text-left">
-
-                  <h1 className="text-2xl mb-4 text-left"> 객관화된 기사에요 </h1>
+                  <h1 className="text-2xl mb-4 text-left">
+                    {" "}
+                    객관화된 기사에요{" "}
+                  </h1>
                   <p className="mb-4 text-left">{item.text}</p>
-                  <h2 className="text-2xl mb-4 text-left">GPT는 아래와 같은 이유로 수정했어요.</h2>
+                  <h2 className="text-2xl mb-4 text-left">
+                    GPT는 아래와 같은 이유로 수정했어요.
+                  </h2>
 
                   <Correction correction={splited}></Correction>
                 </div>
