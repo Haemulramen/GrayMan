96d02745ad7b2f66c4871a187107792529a2a368
Ahn <66175689+ahnavocado@users.noreply.github.com>
2024-06-13 13:01:01 +0900

    ADD : added comment part

--- frontend/src/app/detail/[id]/page.js
+++ frontend/src/app/detail/[id]/page.js
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import { getServerSideProps } from "./lib/fetch_db";
 import Correction from "./layout/correction";
@@ -16,7 +18,7 @@ export default function Detail(props) {
 
   return (
     <main className="grid grid-cols-12 gap-4 py-20 bg-white">
-      <div className="col-span-4 p-4 text-left">
+      <div className="col-span-8 p-4 text-left">
         <div>
           {summaryData.then((response) =>
             response.props.data.map((item, index) => {
@@ -24,23 +26,17 @@ export default function Detail(props) {
               let splited = correction.split(/\s*(?=\d+\.\s)/);
               return (
                 <div key={index} className="mb-6 text-left">
-                  <h1 className="text-2xl mb-4 text-left">
-                    {" "}
-                    객관화된 기사에요{" "}
-                  </h1>
+                  <h1 className="text-2xl mb-4 text-left"> 객관화된 기사에요 </h1>
                   <p className="mb-4 text-left">{item.text}</p>
                   <h2 className="text-2xl mb-4 text-left">
                     GPT는 아래와 같은 이유로 수정했어요.
                   </h2>
-
                   <Correction correction={splited}></Correction>
                 </div>
               );
             })
           )}
         </div>
-      </div>
-      <div className="col-span-4 p-4 text-left">
         <div>
           {originData.then((response) =>
             response.props.data.map((item, index) => {
@@ -55,10 +51,12 @@ export default function Detail(props) {
             })
           )}
         </div>
+        <div className="grid gap-4 py-20">
+          <App />
+          <Statistics />
+        </div>
       </div>
-      <div className="grid gap-4 py-20 col-span-12">
-        <App />
-        <Statistics />
+      <div className="col-span-4 p-4 text-left">
         <Comments />
       </div>
     </main>
