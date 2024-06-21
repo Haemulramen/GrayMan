72a883f52ac3245f791e851399735c1deb8fb066
Haemulramen <apple11078@gmail.com>
2024-06-21 22:11:34 +0900

    FEAT: commit

--- articles/urls.py
+++ articles/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('delete/', delete_article, name='delete_article'),
     path('sum/<int:id>/', summarize, name='summarize'),
     path('chatting/', chatting, name = 'chatting'),
-    path('<int:id>/', article_comment_list, name='article_comment_list')
+    path('<int:id>/', article_comment_list, name='article_comment_list'),
+    path('user_article/',user_article_input, name='user_article_input'),
 ]
